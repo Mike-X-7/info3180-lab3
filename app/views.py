17a3348703d477ef404080799e377d045011617f
Michael Green <likemike11@yahoo.com>
2016-02-14 09:40:47 +0000

    Customized my app

---
+++ app/views.py
@@ -0,0 +1,106 @@
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+
+This file creates your application.
+"""
+
+from app import app
+from flask import render_template, request, redirect, url_for
+import smtplib
+
+
+###
+# Routing for your application.
+###
+
+@app.route('/')
+def default():
+    """Render website's contact page."""
+    return render_template('contact.html')
+
+
+@app.route('/about/')
+def about():
+    """Render the website's about page."""
+    return render_template('about.html')
+  
+  
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    """Render the website's about page."""
+    if request.method=='POST':
+      fromname = request.form['name']
+      fromaddr = request.form['email']
+      subject = request.form['subject']
+      msg = request.form['message']
+      sendemail(fromname, fromaddr, subject, msg)
+    return render_template('contact.html')
+  
+def sendemail(fromname, fromaddr, subject, msg):
+    """Sends email from contact page."""
+    message = """From: {} <{}>
+    To: {} <{}>
+    Subject: {}
+    {}
+    """
+    
+    # Credentials (needed)
+    # username = '<youremailaddress>' Gmail
+    # #password = '{youremailapppassword}' 
+
+    username = 'likemike303@gmail.com'       #INSERT email address
+    password = ''                            #INSERT email app passowrd
+
+    toname = 'You'                               #RECIPIENT NAME
+    toaddr = 'likemike11@yahoo.com'              #RECIPIENT ADDRESS
+
+    messagetosend = message.format(
+     fromname,
+     fromaddr,
+     toname,
+     toaddr,
+     subject,
+     msg)
+
+    # The actual mail send
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(fromaddr, toaddr, messagetosend)
+    server.quit()
+  
+
+
+
+###
+# The functions below should be applicable to all Flask apps.
+###
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0",port="8080")
